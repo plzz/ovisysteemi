13548602048196fccf8b840b9a9f1878d848017e
Jussi Peltola <plz@plz.fi>
2016-10-24 02:22:38 +0300

    Actually define MAIN_MOTOR_MAX_SPEED

--- ovi.c
+++ ovi.c
@@ -5,6 +5,7 @@
 
 int const MAIN_MOTOR_PWM_TOP = 255;
 int const MAIN_MOTOR_MIN_SPEED = 30;
+int const MAIN_MOTOR_MAX_SPEED = 255;
 int const MAGNET_OPEN_WAIT = 5;		// 10ths of a second
 
 // Hardware abstraction layer
