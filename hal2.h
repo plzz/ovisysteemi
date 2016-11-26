b491a3e6156e450810e3d9a8c2d59365606dd208
Jussi Peltola <plz@plz.fi>
2016-11-26 15:17:08 +0200

    Remove functions that are private.

--- hal2.h
+++ hal2.h
@@ -15,8 +15,6 @@ void init(void);
 void main_motor_stop();
 void main_motor_cw_open(uint8_t speed);
 void main_motor_ccw_close(uint8_t speed);
-void set_main_motor_speed(int speed);
-int get_main_motor_speed();
 void aux_motor_stop();
 void aux_motor_cw_close();
 void aux_motor_ccw_open();
