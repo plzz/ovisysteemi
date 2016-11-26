69e064987d11e80f636ac1a03d08ad89b6d860f9
Jussi Peltola <plz@plz.fi>
2016-11-26 15:56:16 +0200

    Don't initialize extern declarations

--- hal2.h
+++ hal2.h
@@ -3,12 +3,12 @@
 
 // Mechanical constants
 
-extern int const MAIN_MOTOR_PWM_TOP = 255;
-extern int const MAIN_MOTOR_BRAKE_SPEED = 120;
-extern int const MAIN_MOTOR_MIN_SPEED = 150;
-extern int const MAIN_MOTOR_MED_SPEED = 254;
-extern int const MAIN_MOTOR_MAX_SPEED = 255;
-extern int const MAGNET_OPEN_WAIT = 5;		// 10ths of a second
+extern int const MAIN_MOTOR_PWM_TOP;
+extern int const MAIN_MOTOR_BRAKE_SPEED;
+extern int const MAIN_MOTOR_MIN_SPEED;
+extern int const MAIN_MOTOR_MED_SPEED;
+extern int const MAIN_MOTOR_MAX_SPEED;
+extern int const MAGNET_OPEN_WAIT;
 
 void init(void);
 
