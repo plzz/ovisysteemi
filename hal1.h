d199f01b05a155a9f8f615afa2614656f4914415
Jussi Peltola <plz@plz.fi>
2016-11-26 14:47:04 +0200

    Move constants to .c and make them extern in .h

--- hal1.h
+++ hal1.h
@@ -3,12 +3,12 @@
 
 // Configurable constants
 
-int const MAIN_MOTOR_PWM_TOP = 255;
-int const MAIN_MOTOR_BRAKE_SPEED = 120;
-int const MAIN_MOTOR_MIN_SPEED = 150;
-int const MAIN_MOTOR_MED_SPEED = 254;
-int const MAIN_MOTOR_MAX_SPEED = 255;
-int const MAGNET_OPEN_WAIT = 5;		// 10ths of a second
+extern int const MAIN_MOTOR_PWM_TOP;
+extern int const MAIN_MOTOR_BRAKE_SPEED;
+extern int const MAIN_MOTOR_MIN_SPEED;
+extern int const MAIN_MOTOR_MED_SPEED;
+extern int const MAIN_MOTOR_MAX_SPEED;
+extern int const MAGNET_OPEN_WAIT;
 
 void init(void);
 
